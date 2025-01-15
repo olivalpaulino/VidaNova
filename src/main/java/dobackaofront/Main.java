27efefb9f192acc3f422c3a9e591b6c51a12c04d
Olival Paulino <olivalpaulinocc@gmail.com>
2025-01-14 22:22:12 -0300

    atualizacao do menu interativo com scanner

--- src/main/java/dobackaofront/Main.java
+++ src/main/java/dobackaofront/Main.java
@@ -1,8 +1,10 @@
 package dobackaofront;
 
 import dobackaofront.controller.Banco;
+import dobackaofront.model.Endereco;
 import dobackaofront.model.Medico;
 import dobackaofront.model.Paciente;
+import dobackaofront.model.Telefone;
 
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -29,7 +31,9 @@ public class Main {
                 m.cadastrarMedico(b,conexao);
                 conexao.close();
             } else if (opcao == 2) {
-                //m.cadastrarPaciente(b,conexao);
+                conexao = b.conectar();
+                m.cadastrarPaciente(b,conexao);
+                b.desconectar(conexao);
             } else if (opcao == 3) {
                 //m.cadastrarAtendimento(b,conexao);
             } else if(opcao == 0) {
@@ -51,5 +55,52 @@ public class Main {
 
         System.out.println(medico.toString());
         b.adicionar(medico, conexao);
+    }
+
+    public void cadastrarPaciente(Banco b, Connection conexao) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Insira o Nome do paciente: ");
+        String nome = sc.nextLine();
+
+        System.out.println("Insira o Cpf do paciente: ");
+        String cpf = sc.nextLine();
+
+        Paciente paciente = new Paciente(nome, cpf);
+
+        System.out.println("Informe a rua do paciente: ");
+        String rua = sc.nextLine();
+
+        System.out.println("Informe o bairro do paciente: ");
+        String bairro = sc.nextLine();
+
+        System.out.println("Informe numero da casa do paciente: ");
+        int numeroCasa = sc.nextInt();
+
+        Endereco endereco = new Endereco(rua, bairro, numeroCasa);
+
+        System.out.println("Deseja cadastrar um telefone para contato? 0-Não, 1-SIM");
+        int opcao = sc.nextInt();
+        ArrayList<Telefone> telefones = new ArrayList<>();
+
+        if (opcao == 1) {
+            while(opcao != 0) {
+                System.out.println("Informe o numero do telefone do paciente: ");
+                sc = new Scanner(System.in);
+                String numeroTelefone = sc.nextLine();
+
+                Telefone telefone = new Telefone(numeroTelefone);
+                telefones.add(telefone);
+
+                System.out.println("Deseja cadastrar um novo telefone para contato? 0-Não, 1-SIM");
+                sc = new Scanner(System.in);
+                opcao = sc.nextInt();
+            }
+
+            b.adicionar(paciente,endereco,telefones,conexao);
+        } else {
+            Telefone telefone = new Telefone("");
+            b.adicionar(paciente,endereco,telefones,conexao);
+        }
     }
 }

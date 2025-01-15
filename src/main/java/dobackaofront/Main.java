f84bb897344a31582d062109073246fbf4fe41b0
Olival Paulino <olivalpaulinocc@gmail.com>
2025-01-14 21:29:11 -0300

    adicionando menu dinamico com scanner

--- src/main/java/dobackaofront/Main.java
+++ src/main/java/dobackaofront/Main.java
@@ -7,18 +7,49 @@ import dobackaofront.model.Paciente;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
         Banco b = new Banco();
-        Connection conexao = b.conectar();
+        Connection conexao;
+        Main m = new Main();
+        //b.executarScript(conexao); //b.executarBackup(conexao); //b.backupGeral(conexao);   //b.desconectar(conexao);
+        // b.backupGeral(conexao);
 
-        //b.executarScript(conexao);
-        //b.executarBackup(conexao);
+        int opcao = 1;
+        Scanner sc;
+        while(opcao != 0) {
+            System.out.println("1-Cadastramento de Médicos\n2-Cadastramento de Paciente\n3-Cadastramento de Atendimento\n0-Sair");
+            sc = new Scanner(System.in);
+            opcao = sc.nextInt();
 
+            if (opcao == 1) {
+                conexao = b.conectar();
+                m.cadastrarMedico(b,conexao);
+                conexao.close();
+            } else if (opcao == 2) {
+                //m.cadastrarPaciente(b,conexao);
+            } else if (opcao == 3) {
+                //m.cadastrarAtendimento(b,conexao);
+            } else if(opcao == 0) {
+                System.out.println("O Programa foi finalizado com sucesso!");
+            }
+        }
+    }
 
-        //b.backupGeral(conexao);
-        //b.desconectar(conexao);
-        b.backupGeral(conexao);
+    public void cadastrarMedico(Banco b, Connection conexao) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Insira o Nome do médico: ");
+        String nome = sc.nextLine();
+
+        System.out.println("Insira o CRM do médico: ");
+        String crm = sc.nextLine();
+
+        Medico medico = new Medico(nome,crm);
+
+        System.out.println(medico.toString());
+        b.adicionar(medico, conexao);
     }
 }
